30f70341329aea8e1bce6c0ff66bfbcf1f9174c7
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-04-28 11:04:03 -0300

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+function Theme(){
+    if  (document.body.style.backgroundColor == 'aliceblue') //igual
+        {document.body.style.backgroundColor = 'rgb(90, 89, 89)' //recebe
+        document.body.id.
+        var b = document.getElementById('botao')
+        b.style = 'background-color:aliceblue;color:black'
+        b.value = '☀️'
+        
+        }
+
+    else
+        {document.body.style.backgroundColor = 'aliceblue'
+        var b = document.getElementById('botao')
+        b.style = 'background-color:rgb(90, 89, 89)'
+        b.value = '🌙'
+    }
+}
